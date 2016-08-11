a9ad47ad65a25cbf0ddb8b640623fcc6875ee791
xznbum <xeisenberg@outloo.com>
2016-08-12 04:53:52 +0500

    report

--- src/Repository/ReportRepository.php
+++ src/Repository/ReportRepository.php
@@ -18,29 +18,49 @@ class ReportRepository extends AbstractRepository
         $date = $report->date . ' 2016';
         $date = date('Y-m', strtotime($date));
         $query = $this->dbConnection->fetchArray(
-          'SELECT SUM(sum), COUNT(*) FROM log WHERE id_account = ? AND id_category = ? AND DATE_FORMAT(date, "%Y-%m") = ?',
-            [$report->id_account, $report->id_category, $date]
+            'SELECT sum, avg_amount, start_amount, end_amount FROM report WHERE id_account = ? AND id_category = ? AND date = ? AND id_user = ? LIMIT 1',
+            [$report->id_account, $report->id_category, $date, $report->id_user]
         );
-
-        if ($query[1] != 0)
+        if($query[0] !== null)
         {
             $report->sum = $query[0];
-            $report->avg_amount = $query[0]/$query[1];
+            $report->avg_amount = $query[1];
+            $report->start_amount = $query[2];
+            $report->end_amount = $query[3];
+        } else
+        {
+            $query = $this->dbConnection->fetchArray(
+                'SELECT SUM(sum), COUNT(*) FROM log WHERE id_account = ? AND id_category = ? AND DATE_FORMAT(date, "%Y-%m") = ?',
+                [$report->id_account, $report->id_category, $date]
+            );
+
+            if ($query[1] != 0)
+            {
+                $report->sum = $query[0];
+                $report->avg_amount = $query[0]/$query[1];
+            }
+
+            $query = $this->dbConnection->fetchArray(
+                'SELECT start_amount FROM log WHERE id_account = ? AND id_category = ? AND DATE_FORMAT(date, "%Y-%m") = ? ORDER BY date LIMIT 1',
+                [$report->id_account, $report->id_category, $date]
+            );
+
+            $report->start_amount = $query[0];
+
+            $query = $this->dbConnection->fetchArray(
+                'SELECT end_amount FROM log WHERE id_account = ? AND id_category = ? AND DATE_FORMAT(date, "%Y-%m") = ? ORDER BY date DESC LIMIT 1',
+                [$report->id_account, $report->id_category, $date]
+            );
+            $report->end_amount = $query[0];
+
+            $this->dbConnection->executeQuery(
+                'INSERT INTO report (date, id_user, id_account, id_category, sum, avg_amount, start_amount, end_amount) VALUES (?, ?, ?, ?, ?, ?, ?, ?)',
+                [$date, $report->id_user, $report->id_account, $report->id_category, $report->sum, $report->avg_amount, $report->start_amount, $report->end_amount]
+            );
+
+
         }
 
-        $query = $this->dbConnection->fetchArray(
-            'SELECT start_amount FROM log WHERE id_account = ? AND id_category = ? AND DATE_FORMAT(date, "%Y-%m") = ? ORDER BY date LIMIT 1',
-            [$report->id_account, $report->id_category, $date]
-        );
-
-        $report->start_amount = $query[0];
-
-        $query = $this->dbConnection->fetchArray(
-            'SELECT end_amount FROM log WHERE id_account = ? AND id_category = ? AND DATE_FORMAT(date, "%Y-%m") = ? ORDER BY date DESC LIMIT 1',
-            [$report->id_account, $report->id_category, $date]
-        );
-
-        $report->end_amount = $query[0];
         return $report;
     }
 }
